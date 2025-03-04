028774fc068667be7c3fc731b322e984ce77c86f
Luis Galvan <luis_galvan221@hotmail.com>
2025-03-03 21:28:10 -0600

    add carrousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "../src/Styles/Home.css";
 import Navbar from "./Components/Navbar";
 import Quote from "./Components/Quote";
+import Follow from "./Components/Follow";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Quote />
+      <Follow />
     </div>
   );
 }
