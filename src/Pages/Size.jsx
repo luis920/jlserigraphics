f97d97de0f2080ada20c76c714092b3047333a9c
Luis Galvan <luis_galvan221@hotmail.com>
2025-03-05 07:35:40 -0600

    shirt polos added

--- src/Pages/Size.jsx
+++ src/Pages/Size.jsx
@@ -1,6 +1,7 @@
 import algodon1 from "../img/algodon1.jpg";
 import algodon2 from "../img/algodon2.jpg";
 import dryfit from "../img/dry-fit.jpg";
+import polos from "../img/polos.jpg";
 import "../Styles/Size.css";
 
 const Size = () => {
@@ -18,6 +19,12 @@ const Size = () => {
         </div>
       </div>
       <h1 className="text-center text-light mt-3 fw-bold">Playera Dry-fit </h1>
+      <div className="d-flex flex-wrap justify-content-center">
+        <div className="card-dama-caballero col-6 col-12">
+          <img src={dryfit} alt="dry-fit adulto" className="w-100" />
+        </div>
+      </div>
+      <h1 className="text-center text-light mt-3 fw-bold">Tipo polo </h1>
       <div className="d-flex flex-wrap justify-content-center">
         <div className="card-dama-caballero col-6 col-12">
           <img src={dryfit} alt="dry-fit adulto" className="w-100" />
