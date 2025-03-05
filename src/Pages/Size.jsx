d66d39fc30fdc0c355935bd0056d0666239da0c4
Luis Galvan <luis_galvan221@hotmail.com>
2025-03-05 07:37:18 -0600

    dress shirt added

--- src/Pages/Size.jsx
+++ src/Pages/Size.jsx
@@ -2,6 +2,7 @@ import algodon1 from "../img/algodon1.jpg";
 import algodon2 from "../img/algodon2.jpg";
 import dryfit from "../img/dry-fit.jpg";
 import polos from "../img/polos.jpg";
+import camisas from "../img/camisa.jpg";
 import "../Styles/Size.css";
 
 const Size = () => {
@@ -27,7 +28,13 @@ const Size = () => {
       <h1 className="text-center text-light mt-3 fw-bold">Tipo polo </h1>
       <div className="d-flex flex-wrap justify-content-center">
         <div className="card-dama-caballero col-6 col-12">
-          <img src={dryfit} alt="dry-fit adulto" className="w-100" />
+          <img src={polos} alt="dry-fit adulto" className="w-100" />
+        </div>
+      </div>
+      <h1 className="text-center text-light mt-3 fw-bold">Camisas </h1>
+      <div className="d-flex flex-wrap justify-content-center">
+        <div className="card-dama-caballero col-6 col-12">
+          <img src={polos} alt="dry-fit adulto" className="w-100" />
         </div>
       </div>
     </div>
