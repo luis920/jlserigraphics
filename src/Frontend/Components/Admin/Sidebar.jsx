468e2d9060eb11389cd2944297085d0aa57f6fd5
Luis Galvan <luis_galvan221@hotmail.com>
2025-03-09 19:20:44 -0600

    add dash oard admin

--- src/Frontend/Components/Admin/Sidebar.jsx
+++ src/Frontend/Components/Admin/Sidebar.jsx
@@ -15,13 +15,8 @@ const Sidebar = () => {
   return (
     <>
       {menuItems.map((item, index) => (
-        <Link
-          key={index}
-          className="tw-inline-flex tw-items-center tw-gap-2 tw-whitespace-nowrap tw-rounded-md tw-text-sm tw-font-medium tw-ring-offset-background tw-transition-colors focus-visible:tw-outline-none focus-visible:tw-ring-2 focus-visible:tw-ring-ring focus-visible:tw-ring-offset-2 tw-disabled:tw-pointer-events-none tw-disabled:tw-opacity-50 [&_svg]:tw-pointer-events-none [&_svg]:tw-size-4 [&_svg]:tw-shrink-0 hover:tw-bg-accent hover:tw-text-accent-foreground tw-h-10 tw-px-4 tw-py-2 tw-w-full tw-justify-start"
-          to={item.path}
-          onClick={() => setIsOpen(false)}
-        >
-          <item.icon className="tw-mr-2 tw-h-4 tw-w-4" />
+        <Link key={index} className="text-light" to={item.path}>
+          {item.icon}
           {item.label}
         </Link>
       ))}
