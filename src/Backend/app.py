86b8ce4543f9bdd899d72522c13f17a6b851cf5a
Luis Galvan <luis_galvan221@hotmail.com>
2025-03-15 21:44:50 -0600

    changes in app.py

--- src/Backend/app.py
+++ src/Backend/app.py
@@ -1,192 +1,192 @@
-# from flask import Flask, request, jsonify
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_marshmallow import Marshmallow
-# from flask_cors import CORS
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 
 
-# # Inicialización de la aplicación Flask
-# app = Flask(__name__)
-# CORS(app)
+# Inicialización de la aplicación Flask
+app = Flask(__name__)
+CORS(app)
 
-# # Configuración de la base de datos
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/flaskmysql'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Desactivar modificaciones de seguimiento
+# Configuración de la base de datos
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/flaskmysql'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Desactivar modificaciones de seguimiento
 
-# # Inicialización de SQLAlchemy y Marshmallow
-# db = SQLAlchemy(app)
-# ma = Marshmallow(app)
+# Inicialización de SQLAlchemy y Marshmallow
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-# # Definición del modelo de la base de datos
-# class Playera(db.Model):  
-#     id = db.Column(db.Integer, primary_key=True)
-#     titulo = db.Column(db.String(100))
-#     descripcion = db.Column(db.String(100))
-#     precio = db.Column(db.Integer)
-#     imagen= db.Column(db.String(100))
+# Definición del modelo de la base de datos
+class Playera(db.Model):  
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100))
+    descripcion = db.Column(db.String(100))
+    precio = db.Column(db.Integer)
+    imagen= db.Column(db.String(100))
 
-#     def __init__(self, titulo, descripcion, precio, imagen):
-#         self.titulo = titulo
-#         self.descripcion = descripcion
-#         self.precio = precio
-#         self.imagen= imagen
+    def __init__(self, titulo, descripcion, precio, imagen):
+        self.titulo = titulo
+        self.descripcion = descripcion
+        self.precio = precio
+        self.imagen= imagen
 
-# class Pedidos(db.Model):  
-#     id = db.Column(db.Integer, primary_key=True)
-#     cliente = db.Column(db.String(100))
-#     tipo_prenda = db.Column(db.String(100))
-#     cantidad = db.Column(db.Integer)
-#     fecha_entrega= db.Column(db.String)
-#     precio = db.Column(db.Float)
-#     total = db.Column(db.Float)
-#     estado_pedido = db.Column(db.String(100))
+class Pedidos(db.Model):  
+    id = db.Column(db.Integer, primary_key=True)
+    cliente = db.Column(db.String(100))
+    tipo_prenda = db.Column(db.String(100))
+    cantidad = db.Column(db.Integer)
+    fecha_entrega= db.Column(db.String)
+    precio = db.Column(db.Float)
+    total = db.Column(db.Float)
+    estado_pedido = db.Column(db.String(100))
 
     
 
-#     def __init__(self, cliente, tipo_prenda, cantidad, fecha_entrega,precio,estado_pedido):
-#         self.cliente = cliente
-#         self.tipo_prenda = tipo_prenda
-#         self.cantidad= cantidad
-#         self.fecha_entrega= fecha_entrega
-#         self.precio = precio
-#         self.total = float(precio)*int(cantidad)
-#         self.estado_pedido = estado_pedido
+    def __init__(self, cliente, tipo_prenda, cantidad, fecha_entrega,precio,estado_pedido):
+        self.cliente = cliente
+        self.tipo_prenda = tipo_prenda
+        self.cantidad= cantidad
+        self.fecha_entrega= fecha_entrega
+        self.precio = precio
+        self.total = float(precio)*int(cantidad)
+        self.estado_pedido = estado_pedido
 
-# class Clientes(db.Model):  
-#     id = db.Column(db.Integer, primary_key=True)
-#     nombre = db.Column(db.String(100))
-#     direccion = db.Column(db.String(100))
-#     telefono = db.Column(db.String(100))
+class Clientes(db.Model):  
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100))
+    direccion = db.Column(db.String(100))
+    telefono = db.Column(db.String(100))
 
-#     def __init__(self, nombre,direccion,telefono):
-#         self.nombre= nombre
-#         self.direccion = direccion
-#         self.telefono= telefono
+    def __init__(self, nombre,direccion,telefono):
+        self.nombre= nombre
+        self.direccion = direccion
+        self.telefono= telefono
 
-# class Cotizaciones(db.Model):  
-#     id = db.Column(db.Integer, primary_key=True)
-#     nombre_del_cliente = db.Column(db.String(100))
-#     direccion_cliente = db.Column(db.String(100))
-#     telefono_cliente = db.Column(db.String(100))
-#     tipo_de_prenda= db.Column(db.String(100))
-#     cantidad_piezas = db.Column(db.String(100))
+class Cotizaciones(db.Model):  
+    id = db.Column(db.Integer, primary_key=True)
+    nombre_del_cliente = db.Column(db.String(100))
+    direccion_cliente = db.Column(db.String(100))
+    telefono_cliente = db.Column(db.String(100))
+    tipo_de_prenda= db.Column(db.String(100))
+    cantidad_piezas = db.Column(db.String(100))
    
 
-#     def __init__(self, nombre_del_cliente,direccion_cliente,telefono_cliente,tipo_de_prenda,cantidad_piezas):
-#         self.nombre_del_cliente= nombre_del_cliente
-#         self.direccion_cliente = direccion_cliente
-#         self.telefono_cliente= telefono_cliente
-#         self.tipo_de_prenda= tipo_de_prenda
-#         self.cantidad_piezas= cantidad_piezas
+    def __init__(self, nombre_del_cliente,direccion_cliente,telefono_cliente,tipo_de_prenda,cantidad_piezas):
+        self.nombre_del_cliente= nombre_del_cliente
+        self.direccion_cliente = direccion_cliente
+        self.telefono_cliente= telefono_cliente
+        self.tipo_de_prenda= tipo_de_prenda
+        self.cantidad_piezas= cantidad_piezas
     
 
-# # Esquema de Marshmallow para serializar y deserializar
-# class PlayeraSchema(ma.SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Playera
-#         fields = ('id', 'titulo', 'descripcion', 'precio', 'imagen')
+# Esquema de Marshmallow para serializar y deserializar
+class PlayeraSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Playera
+        fields = ('id', 'titulo', 'descripcion', 'precio', 'imagen')
 
-# class PedidosSchema(ma.SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Pedidos
-#         fields = ('id', 'cliente', 'tipo_prenda', 'cantidad', 'fecha_entrega', 'precio', 'total', 'estado_pedido')
+class PedidosSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Pedidos
+        fields = ('id', 'cliente', 'tipo_prenda', 'cantidad', 'fecha_entrega', 'precio', 'total', 'estado_pedido')
 
-# class ClientesSchema(ma.SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Clientes
-#         fields = ('id','nombre','direccion','telefono')
+class ClientesSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Clientes
+        fields = ('id','nombre','direccion','telefono')
 
         
 
 
 
-# # Crear una instancia del esquema
-# playera_schema = PlayeraSchema()
-# playeras_schema = PlayeraSchema(many=True)
+# Crear una instancia del esquema
+playera_schema = PlayeraSchema()
+playeras_schema = PlayeraSchema(many=True)
 
-# pedido_schema = PedidosSchema()
-# pedidos_schema = PedidosSchema(many=True)
+pedido_schema = PedidosSchema()
+pedidos_schema = PedidosSchema(many=True)
 
-# cliente_schema = ClientesSchema()
-# clientes_schema = ClientesSchema(many=True)
+cliente_schema = ClientesSchema()
+clientes_schema = ClientesSchema(many=True)
 
-# # Crear las tablas de la base de datos (mejor hacerlo solo una vez)
-# with app.app_context():
-#     db.create_all()
+# Crear las tablas de la base de datos (mejor hacerlo solo una vez)
+with app.app_context():
+    db.create_all()
 
-# # Endpoint para crear una nueva playera
-# @app.route('/playera', methods=['POST'])
-# def crear_playera():
-#     # Obtener los datos del JSON
-#     titulo = request.json['titulo']
-#     descripcion = request.json['descripcion']
-#     precio = request.json['precio']
-#     imagen= request.json['imagen']
+# Endpoint para crear una nueva playera
+@app.route('/playera', methods=['POST'])
+def crear_playera():
+    # Obtener los datos del JSON
+    titulo = request.json['titulo']
+    descripcion = request.json['descripcion']
+    precio = request.json['precio']
+    imagen= request.json['imagen']
 
-#     # Crear una nueva instancia de Playera
-#     nueva_playera = Playera(titulo, descripcion, precio, imagen)
+    # Crear una nueva instancia de Playera
+    nueva_playera = Playera(titulo, descripcion, precio, imagen)
 
-#     # Agregar la nueva playera a la base de datos
-#     db.session.add(nueva_playera)
-#     db.session.commit()
+    # Agregar la nueva playera a la base de datos
+    db.session.add(nueva_playera)
+    db.session.commit()
 
-#     # Serializar la nueva playera y devolverla en la respuesta
-#     return jsonify(playera_schema.dump(nueva_playera)), 201
+    # Serializar la nueva playera y devolverla en la respuesta
+    return jsonify(playera_schema.dump(nueva_playera)), 201
 
-# # PEDIDOS
-# @app.route('/pedido', methods=['POST'])
-# def crear_pedido():
+# PEDIDOS
+@app.route('/pedido', methods=['POST'])
+def crear_pedido():
     
-#     cliente = request.json['cliente']
-#     tipo_prenda = request.json['tipo_prenda']
-#     cantidad = request.json['cantidad']
-#     fecha_entrega= request.json['fecha_entrega']
-#     precio= request.json['precio']
-#     estado_pedido= request.json['estado_pedido']
+    cliente = request.json['cliente']
+    tipo_prenda = request.json['tipo_prenda']
+    cantidad = request.json['cantidad']
+    fecha_entrega= request.json['fecha_entrega']
+    precio= request.json['precio']
+    estado_pedido= request.json['estado_pedido']
 
-#     nuevo_pedido = Pedidos(cliente, tipo_prenda, cantidad, fecha_entrega,precio,estado_pedido)
+    nuevo_pedido = Pedidos(cliente, tipo_prenda, cantidad, fecha_entrega,precio,estado_pedido)
 
-#     db.session.add(nuevo_pedido)
-#     db.session.commit()
+    db.session.add(nuevo_pedido)
+    db.session.commit()
 
-#     return jsonify(pedido_schema.dump(nuevo_pedido)), 201
+    return jsonify(pedido_schema.dump(nuevo_pedido)), 201
 
-# @app.route('/pedidos', methods=['GET'])
-# def obtener_pedidos():
+@app.route('/pedidos', methods=['GET'])
+def obtener_pedidos():
     
-#     pedidos = Pedidos.query.all()
+    pedidos = Pedidos.query.all()
 
-#     return jsonify(pedidos_schema.dump(pedidos)), 200
+    return jsonify(pedidos_schema.dump(pedidos)), 200
 
-# #CLIENTES
+#CLIENTES
 
-# @app.route('/cliente', methods=['POST'])
-# def crear_cliente():
-#     # Obtener los datos del JSON
-#     nombre = request.json['nombre']
-#     direccion = request.json['direccion']
-#     telefono = request.json['telefono']
+@app.route('/cliente', methods=['POST'])
+def crear_cliente():
+    # Obtener los datos del JSON
+    nombre = request.json['nombre']
+    direccion = request.json['direccion']
+    telefono = request.json['telefono']
     
-#     nuevo_cliente = Clientes(nombre, direccion, telefono,)
+    nuevo_cliente = Clientes(nombre, direccion, telefono,)
 
-#     db.session.add(nuevo_cliente)
-#     db.session.commit()
+    db.session.add(nuevo_cliente)
+    db.session.commit()
 
-#     return jsonify(cliente_schema.dump(nuevo_cliente)), 201
+    return jsonify(cliente_schema.dump(nuevo_cliente)), 201
 
-# @app.route('/clientes', methods=['GET'])
+@app.route('/clientes', methods=['GET'])
 
-# def obtener_clientes():
+def obtener_clientes():
 
-#     clientes = Clientes.query.all()
+    clientes = Clientes.query.all()
 
-#     return jsonify(clientes_schema.dump(clientes)),200
-
-
+    return jsonify(clientes_schema.dump(clientes)),200
 
 
 
 
-# # Ejecutar la aplicación
-# if __name__ == '__main__':
-#     app.run(debug=True)
+
+
+# Ejecutar la aplicación
+if __name__ == '__main__':
+    app.run(debug=True)
