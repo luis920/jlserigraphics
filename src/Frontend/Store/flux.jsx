a17a817cfe264ee796f2148861817ba09b757895
Luis Galvan <luis_galvan221@hotmail.com>
2025-03-15 19:54:47 -0600

    add routes clients in flux

--- src/Frontend/Store/flux.jsx
+++ src/Frontend/Store/flux.jsx
@@ -4,7 +4,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
-      clients: [],
+      clientes: [],
       pedidos: [],
     },
     actions: {
@@ -162,6 +162,43 @@ const getState = ({ getStore, getActions, setStore }) => {
           }
         } catch (error) {
           console.error("Error al agregar pedido:", error);
+        }
+      },
+      obtenerClientes: async () => {
+        try {
+          const response = await fetch("http://127.0.0.1:5000/clientes", {});
+          if (response.ok) {
+            const data = await response.json();
+            setStore({ clientes: data });
+            return data;
+          } else {
+            console.error("Error al obtener clientes:", response.status);
+          }
+        } catch (error) {
+          console.error("Error al obtener clientes:", error);
+          return null;
+        }
+      },
+      agregarCliente: async (nuevoCliente) => {
+        try {
+          const response = await fetch("http://127.0.0.1:5000/cliente", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(nuevoCliente),
+          });
+
+          if (response.ok) {
+            const nuevoCliente = await response.json();
+            const store = getStore();
+            setStore({ clientes: [...store.clientes, nuevoCliente] });
+            return nuevoCliente;
+          } else {
+            console.error("Error al agregar cliente:", response.status);
+          }
+        } catch (error) {
+          console.error("Error al agregar cliente:", error);
         }
       },
     },
