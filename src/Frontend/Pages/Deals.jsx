884b7d6eba3f3a6e0aad8bb7bf00c7dd6b4164f9
Luis Galvan <luis_galvan221@hotmail.com>
2025-03-31 18:52:25 -0600

    changes in deals

--- src/Frontend/Pages/Deals.jsx
+++ src/Frontend/Pages/Deals.jsx
@@ -9,9 +9,39 @@ const Deals = () => {
       imagen:
         "https://i5.walmartimages.com/asr/78f3e13f-b452-45fb-a4af-9f578eb0ff0f.da696bdb2fc1e486023ee83b27385f4c.jpeg",
     },
+    {
+      titulo: "Playera algodon estampada a 1 tinta",
+      precio: 105,
+      imagen:
+        "https://i5.walmartimages.com/asr/78f3e13f-b452-45fb-a4af-9f578eb0ff0f.da696bdb2fc1e486023ee83b27385f4c.jpeg",
+    },
+    {
+      titulo: "Playera algodon estampada a 1 tinta",
+      precio: 105,
+      imagen:
+        "https://i5.walmartimages.com/asr/78f3e13f-b452-45fb-a4af-9f578eb0ff0f.da696bdb2fc1e486023ee83b27385f4c.jpeg",
+    },
+    {
+      titulo: "Playera algodon estampada a 1 tinta",
+      precio: 105,
+      imagen:
+        "https://i5.walmartimages.com/asr/78f3e13f-b452-45fb-a4af-9f578eb0ff0f.da696bdb2fc1e486023ee83b27385f4c.jpeg",
+    },
+    {
+      titulo: "Playera algodon estampada a 1 tinta",
+      precio: 105,
+      imagen:
+        "https://i5.walmartimages.com/asr/78f3e13f-b452-45fb-a4af-9f578eb0ff0f.da696bdb2fc1e486023ee83b27385f4c.jpeg",
+    },
+    {
+      titulo: "Playera algodon estampada a 1 tinta",
+      precio: 105,
+      imagen:
+        "https://i5.walmartimages.com/asr/78f3e13f-b452-45fb-a4af-9f578eb0ff0f.da696bdb2fc1e486023ee83b27385f4c.jpeg",
+    },
   ];
   return (
-    <div>
+    <div className="">
       <div className="text-center bg-dark p-2">
         <h1 className="text-light fw-bold">¡TE AYUDAMOS A AHORRAR!</h1>
         <p className="text-light">
@@ -26,15 +56,19 @@ const Deals = () => {
       <h1 className="text-light fw-bold text-center fs-1 mt-3">
         ¡Grandes descuentos te esperan! Mira nuestras ofertas.
       </h1>
-      {ofertas.map((item) => (
-        <div key={item.id} class="card">
-          <div class="image">
-            <img src={item.imagen} alt="" className="img-deals" />
+      <div className="row">
+        {ofertas.map((item, index) => (
+          <div key={index} className="col-md-4 mt-3">
+            <div className="card">
+              <div className="image">
+                <img src={item.imagen} alt="" className="img-deals" />
+              </div>
+              <span className="title">{item.titulo}</span>
+              <span className="price">${item.precio}</span>
+            </div>
           </div>
-          <span class="title">{item.titulo}</span>
-          <span class="price">${item.precio}</span>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
